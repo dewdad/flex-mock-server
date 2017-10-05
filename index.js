f72d235d532b156d99ca41bbfa65c83d12bcf018
roney <ningrao@126.com>
2017-10-05 17:17:34 +0800

    middleware function change to a mixture of path-to-handlers map

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const fs = require('fs');
 const path = require('path');
 const mime = require('mime');
 
+const stdHandlers=require('./standard_handlers.js');
 
 const program=require('commander');
+
+const dataKey=Symbol('FS-DATA');
 
 //function collect(val, memo) {
 //  memo.push(val);
@@ -15,15 +18,38 @@ const program=require('commander');
 //}
 program
 	.option('-d, --debug', 'log debug info')
-	.option('-p, --port', 'server port, default as 3000')
+	.option('-p, --port [port]', 'server port, default as 3000')
 	.option('-c, --cwd [dir]', 'current working directory, default to process.cwd()')
-	.option('-m, --middleware [file]', 'middleware file path; function(req, res); if handled, return true;')
+	.option('-C, --cors ', 'allow cors (option header returns *)')
+	.option('-f, --folder [dir]', 'files serving directory')
+	.option('-m, --map [file]', 'custom response map file path; see below example')
 	.option('-i, --index [file]', 'default html page file name for **existing** folder')
 	.option('-5, --html5 [file]', 'whether to support html5 history api, as webpackDevServer "historyApiFallback", which is a path resorting to when directory **non-exists**; if "true" set to "index", or specify specifically, relative to where server starts;')
 	.option('-r, --root [dir]', 'virtual root directory, as webpack "publicPath", starts with "/", which will be removed when match file;')
-	.option('-M, --mock [dir]', 'api directory, rewrites to "mock";')
 	.parse(process.argv);
 //if <>|[], its bool;
+program.on('--help', function(){
+	console.log(`
+	response map file: 
+		a map of path patterns to data. data can be any types:
+		{number}: it's treated as http code, then handled internally; turn to string if number is returned;
+		{string}: path rewrites;
+		{function}: custom 'pre' handler (equal to {pre:function}), when response data (not undefined) is returned, indicating handled successfully, processing stops.
+		{{post:function}}: custom 'post' handler, modify response
+		{{Symbol('FS-DATA'):*}}: direct return data;
+	example:
+	module.exports={
+		'dir1/dir2/.*':401,
+		'dir1/dir2/':'dir1/abc/dir2',
+		'dir1/dir2/':function(req, res){
+			if(res.params.id==1){
+				res.json({success:false});
+				return true;
+			}
+		},
+	}
+	`);
+});
 
 const log= program.debug? console.log.bind(console) :function(){};
 
@@ -31,10 +57,12 @@ log('options:', program);
 
 const port = program.port || 3000;
 
-
 const cwd = program.cwd || process.cwd();
 
-const middleware=program.middleware?require(path.resolve(cwd, program.middleware)):null;
+const folder=program.folder?path.resolve(cwd, program.folder):cwd;
+
+log('program.map', program.map, path.resolve(cwd, program.map));
+const map=program.map?require(path.resolve(cwd, program.map)):null;
 
 let rootLen=0;
 if(program.root){
@@ -45,24 +73,16 @@ if(program.root){
 	}
 }
 
-let mockLen=0;
-if(program.mock){
-	mockLen=program.mock.length;
-	if(program.mock.endsWith('/')){//user may not input trailing slash;
-		mockLen--;
-		program.mock=program.mock.substr(0, mockLen);
-	}
-}
-
 let Index=program.index||'index.html';
 
 let HistoryIndex;
 if(program.html5){
-	HistoryIndex=program.html5=="true"?Index:program.html5;
+	HistoryIndex=program.html5===true?Index:program.html5;
 	HistoryIndex='/'+HistoryIndex;
+	HistoryIndex=path.join(folder, HistoryIndex);
 	log('none-exist directory resorts to: ', HistoryIndex);
-	if(!fs.existsSync('.'+HistoryIndex)){
-		console.error('this resorted html5 history file does not exist');
+	if(!fs.existsSync(HistoryIndex)){
+		console.error('this resorted html5 history file does not exist', HistoryIndex);
 		process.exit();
 	}
 }
@@ -73,9 +93,16 @@ process.on('SIGINT', function(){
 });
 
 http.createServer(function (req, res) {
-	console.log(`${req.method} ${req.url}`);
-	if(middleware&&middleware(req, res)){
-		console.log('middleware handled');
+	console.log('\x1b[32m%s\x1b[0m',`${req.method} ${req.url}`);
+	//
+	if(program.cors && req.headers.origin){
+		res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+		res.setHeader('Access-Control-Allow-Credentials', 'true');
+		res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers']||'*');
+		res.setHeader('Access-Control-Allow-Methods', req.headers['access-control-request-method']||'*');
+	}
+	if(req.method=='OPTIONS'){
+		res.end();
 		return;
 	}
 	// parse URL
@@ -89,41 +116,100 @@ http.createServer(function (req, res) {
 	}
 	log('pathname', pathname);
 
-	fs.exists('.'+pathname, function (exist) {
-		if(!exist) {
+
+	const posts=[];
+	function end(data){
+		if(posts.length){
+			posts.forEach((el)=>{
+				el(req, res);
+			})
+		}
+		res.end(data);
+	}
+	if(map){
+		let handled=false, target;
+		for(let patn in map){
+			const patnReg=new RegExp(patn);
+			if(patnReg.test(pathname)){
+				console.log('matched pattern:', patn);
+				target=map[patn];
+				if(typeof(target)=='number'){//standard handlers
+					stdHandlers[target](req, res);
+				}else if(typeof(target)=='function'){//custom handler
+					if(!(target(req, res))){
+						continue;//not handled;
+					}
+				}else if(typeof(target)=='string'){//path rewrites;
+					console.log('replace',patnReg, target);
+					pathname=pathname.replace(patnReg, target);
+					break;
+				}else if(typeof(target)=='object'){
+					if(target.post){
+						posts.push(target.post);
+					}
+					if(target.pre){
+						if(!(target.pre(req, res))){
+							continue;//not handled;
+						}
+					} else if(dataKey in target){// return this data directly;
+						if(posts.length){
+							posts.forEach((el)=>{
+								el(req, res);
+							})
+						}
+						end(JSON.stringify(target[dataKey]));
+					}else{
+						continue;
+					}
+				}
+				handled=true;
+				break;
+			}
+		}
+		if(handled){
+			console.log('custom response returned', target);
+			return;
+		}
+	}
+
+
+
+	pathname=path.join(folder,pathname);
+	console.log('pathname', pathname);
+	fs.exists(pathname, function (exist) {
+		if(exist) {
+			// if a directory, then look for index file (directory exists, so not html5 resort scenario)
+			if(fs.statSync(pathname).isDirectory()){
+				if(!pathname.endsWith('/')){
+					pathname+='/';
+				}
+				pathname += Index;
+			}
+		}else{
 			//api rewrite:
-			if(mockLen && pathname.startsWith(program.mock)){
-				pathname=`/mock${pathname.substr(mockLen)}.json`;
-			}else if(HistoryIndex){//resort only when directory;
+			if(HistoryIndex && pathname.indexOf('.')==-1){//resort only when directory;
 				log('resorting...');
 				pathname=HistoryIndex;
 			}else{
 				// if the file is not found, return 404
-				console.log('none:', pathname)
+				console.log('Not found:', pathname)
 				res.statusCode = 404;
-				res.end(`File ${pathname} not found!`);
+				end(`File ${pathname} not found!`);
 				return;
 			}
 		}
-		// if a directory, then look for index file (directory exists, so not html5 resort scenario)
-		if(fs.statSync('.'+pathname).isDirectory()){
-			if(!pathname.endsWith('/')){
-				pathname+='/';
-			}
-			pathname += Index;
-		}
 		console.log('--serve with file:', pathname);
 		// read file from file system
-		fs.readFile('.'+pathname, function(err, data){
+		fs.readFile(pathname, function(err, data){
 			if(err){
 				res.statusCode = 500;
-				res.end(`Error getting the file: ${err}.`);
+				end(`Error getting the file: ${err}.`);
 			} else {
 				// based on the URL path, extract the file extention. e.g. .js, .doc, ...
 				const ext = path.parse(pathname).ext;
 				// if the file is found, set Content-type and send data
 				res.setHeader('Content-type', mime.lookup(ext));
-				res.end(data);
+				end(data);
 			}
 		});
 	});
